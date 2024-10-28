2404a9a2e4b65f5fc2010b2485a2184a1046d881
Piers Karsenbarg <piers.karsenbarg@gmail.com>
2024-10-28 22:13:43 +0000

    Brought back tokyo
    For now until I find something I like better

--- lua/piers/plugins/colourscheme-dracula.lua
+++
@@ -1,9 +0,0 @@
-return {
-	{
-		"dracula/vim",
-		priority = 1000, -- make sure to load this before all the other start plugins
-		init = function()
-			vim.cmd.colorscheme("dracula")
-		end,
-	},
-}

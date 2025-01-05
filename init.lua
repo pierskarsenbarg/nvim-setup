b5354e326a63b50b592118793bb7d581c2572f63
Piers Karsenbarg <piers.karsenbarg@gmail.com>
2025-01-05 11:28:42 +0000

    Usig lazyvim

--- init.lua
+++ init.lua
@@ -1,2 +1,2 @@
-require("piers.core")
-require("piers.lazy")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- lua/piers/core/init.lua
+++
@@ -1,3 +0,0 @@
-require("piers.core.options")
-require("piers.core.keymaps")
-require("piers.core.filetype")
